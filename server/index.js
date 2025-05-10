975ca32c9ebc4e9b3ffdf28892d7630d757662e6
M Nadeem <ghomnaam1234@gmail.com>
2025-05-10 13:51:25 +0500

    PORT Code Updated

--- server/index.js
+++ server/index.js
@@ -420,4 +420,9 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(process.env.PORT || 5000, () => console.log("Server running on port 5000"));
+// ...
+const PORT = process.env.PORT || 5000; // Fallback for local dev
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running  on port ${PORT}`);
+});
+
